8314a9b8978c561cea675f3fe1ffd5140834ad73
kamildobkowski <kamil.dobkowski.03@gmail.com>
2024-04-11 00:06:50 +0200

    Move Markers Edit to different component Change adding marker to Modal

---
+++ src/components/EditTestMarker.jsx
@@ -0,0 +1,76 @@
+import {Accordion, Button, Form, FormGroup, Modal} from "react-bootstrap";
+import {useState} from "react";
+
+function EditTestMarker({ test, setTest }) {
+	const [newMarker, setNewMarker] = useState({name: "", shortName: "", unit: "", lowerNorm: "", higherNorm: ""});
+	const [showModal, setShowModal] = useState(false);
+
+	const handleAddMarker = () => {
+		setTest({...test, markers: [...test.markers, newMarker]});
+		setNewMarker({name: "", shortName: "", unit: "", lowerNorm: "", higherNorm: ""});
+		setShowModal(false);
+	};
+
+	return <>
+		<Accordion>
+			{test.markers.map(m => {
+				return (
+					<Accordion.Item key={m.shortName} eventKey={m.shortName}>
+						<Accordion.Header>{m.name}</Accordion.Header>
+						<Accordion.Body>
+							<FormGroup key={m.shortName}>
+								<Form.Label>Nazwa</Form.Label>
+								<Form.Control defaultValue={m.name} onChange={r => {
+									test.markers.find(x => x.shortName === m.shortName).name = r.target.value;
+								}}></Form.Control>
+								<Form.Label>Jednostka</Form.Label>
+								<Form.Control defaultValue={m.unit} onChange={r => {
+									test.markers.find(x => x.shortName === m.shortName).unit = r.target.value;
+								}}/>
+								<Form.Label>Dolna norma</Form.Label>
+								<Form.Control defaultValue={m.lowerNorm} onChange={r => {
+									test.markers.find(x => x.shortName === m.shortName).lowerNorm = r.target.value;
+								}}/>
+								<Form.Label>Górna norma</Form.Label>
+								<Form.Control defaultValue={m.higherNorm} onChange={r => {
+									test.markers.find(x => x.shortName === m.shortName).higherNorm = r.target.value;
+								}}/>
+							</FormGroup>
+							<Button variant={"danger"} onClick={() => {
+								test.markers = test.markers.filter(x => x.shortName !== m.shortName);
+								setTest({...test});
+							}}>
+								Usuń
+							</Button>
+						</Accordion.Body>
+					</Accordion.Item>
+				)
+			})}
+		</Accordion>
+		<Button variant="success" onClick={() => setShowModal(true)}>Dodaj marker</Button>
+
+		<Modal show={showModal} onHide={() => setShowModal(false)}>
+			<Modal.Header closeButton>
+				<Modal.Title>Dodaj marker</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<FormGroup>
+					<Form.Label>Nazwa</Form.Label>
+					<Form.Control value={newMarker.name} onChange={e => setNewMarker({...newMarker, name: e.target.value})}/>
+					<Form.Label>Jednostka</Form.Label>
+					<Form.Control value={newMarker.unit} onChange={e => setNewMarker({...newMarker, unit: e.target.value})}/>
+					<Form.Label>Dolna norma</Form.Label>
+					<Form.Control value={newMarker.lowerNorm} onChange={e => setNewMarker({...newMarker, lowerNorm: e.target.value})}/>
+					<Form.Label>Górna norma</Form.Label>
+					<Form.Control value={newMarker.higherNorm} onChange={e => setNewMarker({...newMarker, higherNorm: e.target.value})}/>
+				</FormGroup>
+			</Modal.Body>
+			<Modal.Footer>
+				<Button variant="secondary" onClick={() => setShowModal(false)}>Zamknij</Button>
+				<Button variant="primary" onClick={handleAddMarker}>Dodaj marker</Button>
+			</Modal.Footer>
+		</Modal>
+	</>
+}
+
+export default EditTestMarker;
--- src/components/WorkerTestDetails/WorkerTestDetails.jsx
+++ src/components/WorkerTestDetails/WorkerTestDetails.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import {useEffect, useState} from "react";
 import {Accordion, Button, Form, FormGroup, Spinner} from "react-bootstrap";
 import {getWorkerToken} from "../../jwtToken.js";
+import EditTestMarker from "../EditTestMarker.jsx";
 
 function WorkerTestDetails() {
 	const {id} = useParams();
@@ -50,47 +51,7 @@ function WorkerTestDetails() {
 		:
 		<>
 			<h1>{test.name} ({test.shortName})</h1>
-			<Accordion>
-				{test.markers.map(m => {
-					// napisz kod, który z stworzy listę markerów, składającą się z pól tekstowych i jeśli w tym polu zmodyfikuje się tekst to w useState test zmieni się te pole
-					return (
-						<Accordion.Item key={m.shortName} eventKey={m.shortName}>
-							<Accordion.Header>{m.name}</Accordion.Header>
-							<Accordion.Body>
-								<FormGroup key={m.shortName}>
-									<Form.Label>Nazwa</Form.Label>
-									<Form.Control defaultValue={m.name} onChange={r => {
-										test.markers.find(x => x.shortName === m.shortName).name = r.target.value;
-									}}></Form.Control>
-									<Form.Label>Jednostka</Form.Label>
-									<Form.Control defaultValue={m.unit} onChange={r => {
-										test.markers.find(x => x.shortName === m.shortName).unit = r.target.value;
-										console.log(test.markers.find(x => x.shortName === m.shortName))
-									}}/>
-									<Form.Label>Dolna norma</Form.Label>
-									<Form.Control defaultValue={m.lowerNorm} onChange={r => {
-										test.markers.find(x => x.shortName === m.shortName).lowerNorm = r.target.value;
-									}}/>
-									<Form.Label>Górna norma</Form.Label>
-									<Form.Control defaultValue={m.higherNorm} onChange={r => {
-										test.markers.find(x => x.shortName === m.shortName).higherNorm = r.target.value;
-									}}/>
-								</FormGroup>
-								<Button variant={"danger"} onClick={() => {
-									test.markers = test.markers.filter(x => x.shortName !== m.shortName);
-									setTest({...test});
-								}}>
-									Usuń
-								</Button>
-							</Accordion.Body>
-						</Accordion.Item>
-					)
-				})}
-			</Accordion>
-			<Button variant="success" onClick={() => {
-				test.markers.push({name: "", shortName: "", unit: "", lowerNorm: "", higherNorm: ""});
-				setTest({...test});
-			}}>Dodaj marker</Button>
+			<EditTestMarker test={test} setTest={setTest}/>
 			<Button variant="primary" onClick={()=> {
 				updateTest();
 			}}>Ok</Button>
