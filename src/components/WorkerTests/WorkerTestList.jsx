6947c3d6bfa4db87266cb44769929f3818ca6466
kamildobkowski <kamil.dobkowski.03@gmail.com>
2024-04-16 09:28:37 +0200

    Add searchbar to test list

--- src/components/WorkerTests/WorkerTestList.jsx
+++ src/components/WorkerTests/WorkerTestList.jsx
@@ -1,4 +1,4 @@
-import {ListGroup, Spinner, Container, Row, Col, Button, Modal, Pagination} from "react-bootstrap";
+import {ListGroup, Spinner, Container, Row, Col, Button, Modal, Pagination, Form} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {getWorkerToken} from "../../jwtToken.js";
@@ -16,10 +16,16 @@ function WorkerTestList() {
 	const queryParams = new URLSearchParams(location.search);
 	const [page, setPage] = useState(Number(queryParams.get('page') ?? 1));
 
+	const [search, setSearch] = useState('');
+
 	const getTests = async () => {
+		let url = `tests?page=${page}&pageSize=10`;
+		if(search) {
+			url += `&filter=${search}`;
+		}
 		setIsLoading(true);
 		try {
-			const response = await axios.get(`tests?page=${page}&pageSize=2`, {
+			const response = await axios.get(url, {
 				headers: {
 					"Authorization": `Bearer ${getWorkerToken()}`,
 				}
@@ -51,72 +57,78 @@ function WorkerTestList() {
 	useEffect(() => {
 		getTests();
 		console.log(page);
-	}, [page]);
+	}, [page, search]);
 	return (
-		isLoading ?
+		<>
+			<Form>
+				<Form.Label>Wyszukaj</Form.Label>
+				<Form.Control type="text" placeholder="Wyszukaj test" value={search} onChange={(e) => setSearch(e.target.value)}/>
+			</Form>
+		<div>
+			{isLoading ?
 			<Spinner animation="border" role="status">
 				<span className="visually-hidden">Loading...</span>
 			</Spinner>
 			: <Container fluid>
-				<Row>
-					<Col>
-						<h1>Lista Testów</h1>
-						<ListGroup variant="flush">
-							{tests.map(t => {
+			<Row>
+				<Col>
+					<h1>Lista Testów</h1>
+					<ListGroup variant="flush">
+						{tests.map(t => {
+							return (
+								<ListGroup.Item action key={t.id} style={{boxShadow: '0px 0px 10px rgba(0,0,0,0.1)', margin: '10px 0'}} onClick={() => setSelectedTest(t)}>
+									<Row>
+										<Col xs={10}>
+											<h5>{t.name}</h5>
+											<small className="text-muted">{t.shortName}</small>
+										</Col>
+										<Col xs={2} className="d-flex justify-content-end">
+											<Button variant="info" onClick={(e)=>{e.stopPropagation(); navigate(`/lab/test/edit/${t.id}`);}}>Edytuj</Button>
+											<Button variant="danger" onClick={(e)=>{e.stopPropagation(); setTestToDelete(t);}}>Usuń</Button>
+										</Col>
+									</Row>
+								</ListGroup.Item>
+							)
+						})}
+					</ListGroup>
+				</Col>
+			</Row>
+			<Button variant="outline-success" onClick={(e)=> {e.stopPropagation(); navigate(`/lab/test/add`)}}>Dodaj nowy test</Button>
+			<Modal show={selectedTest !== null} onHide={() => setSelectedTest(null)}>
+				<Modal.Header closeButton>
+					<Modal.Title>Szczegóły testu</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					{selectedTest && (
+						<>
+							<h5>{selectedTest.name}</h5>
+							<p>{selectedTest.shortName}</p>
+							{selectedTest.markers.map(m => {
 								return (
-									<ListGroup.Item action key={t.id} style={{boxShadow: '0px 0px 10px rgba(0,0,0,0.1)', margin: '10px 0'}} onClick={() => setSelectedTest(t)}>
-										<Row>
-											<Col xs={10}>
-												<h5>{t.name}</h5>
-												<small className="text-muted">{t.shortName}</small>
-											</Col>
-											<Col xs={2} className="d-flex justify-content-end">
-												<Button variant="info" onClick={(e)=>{e.stopPropagation(); navigate(`/lab/test/edit/${t.id}`);}}>Edytuj</Button>
-												<Button variant="danger" onClick={(e)=>{e.stopPropagation(); setTestToDelete(t);}}>Usuń</Button>
-											</Col>
-										</Row>
-									</ListGroup.Item>
+									<div key={m.shortName}>
+										<h6>{m.name} ({m.unit})</h6>
+									</div>
 								)
 							})}
-						</ListGroup>
-					</Col>
-				</Row>
-				<Button variant="outline-success" onClick={(e)=> {e.stopPropagation(); navigate(`/lab/test/add`)}}>Dodaj nowy test</Button>
-				<Modal show={selectedTest !== null} onHide={() => setSelectedTest(null)}>
-					<Modal.Header closeButton>
-						<Modal.Title>Szczegóły testu</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>
-						{selectedTest && (
-							<>
-								<h5>{selectedTest.name}</h5>
-								<p>{selectedTest.shortName}</p>
-								{selectedTest.markers.map(m => {
-									return (
-										<div key={m.shortName}>
-											<h6>{m.name} ({m.unit})</h6>
-										</div>
-									)
-								})}
-							</>
-						)}
-					</Modal.Body>
-					<Modal.Footer>
-						<Button variant="secondary" onClick={() => setSelectedTest(null)}>Zamknij</Button>
-					</Modal.Footer>
-				</Modal>
-				<Modal show={testToDelete !== null} onHide={() => setTestToDelete(null)}>
-					<Modal.Header closeButton>
-						<Modal.Title>Usuń test</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>
-						Czy na pewno chcesz usunąć test {testToDelete && testToDelete.name}?
-					</Modal.Body>
-					<Modal.Footer>
-						<Button variant="secondary" onClick={() => setTestToDelete(null)}>Nie</Button>
-						<Button variant="danger" onClick={deleteTest}>Tak</Button>
-					</Modal.Footer>
-				</Modal>
+						</>
+					)}
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={() => setSelectedTest(null)}>Zamknij</Button>
+				</Modal.Footer>
+			</Modal>
+			<Modal show={testToDelete !== null} onHide={() => setTestToDelete(null)}>
+				<Modal.Header closeButton>
+					<Modal.Title>Usuń test</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					Czy na pewno chcesz usunąć test {testToDelete && testToDelete.name}?
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={() => setTestToDelete(null)}>Nie</Button>
+					<Button variant="danger" onClick={deleteTest}>Tak</Button>
+				</Modal.Footer>
+			</Modal>
 			<Pagination>
 				<Pagination.First/>
 				<Pagination.Prev/>
@@ -131,7 +143,9 @@ function WorkerTestList() {
 				<Pagination.Next/>
 				<Pagination.Last/>
 			</Pagination>
-			</Container>
+		</Container>}
+		</div>
+		</>
 	)
 }
 
